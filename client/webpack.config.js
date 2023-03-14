280bf2668cf350486db9e9c851354a3af02e8a63
Your Name <you@example.com>
2023-03-13 22:51:20 -0700

    files added

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,11 +2,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
-const { publicDecrypt } = require('crypto');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
@@ -21,7 +18,8 @@ module.exports = () => {
     plugins: [
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'My text editor'
+        title: 'Text Editor',
+        favicon: './favicon.ico',
       }),
       new InjectManifest({
         swSrc: './src-sw.js',
@@ -31,7 +29,7 @@ module.exports = () => {
         fingerprints: false,
         inject: true,
         name: 'just another text editor',
-        short_name: 'jate',
+        short_name: 'J.A.T.E',
         description: 'It is a note taker that uses java script syntax highlighting!',
         background_color: '#40826d',
         theme_color: '#336857',
@@ -41,13 +39,15 @@ module.exports = () => {
           {
             src: path.resolve('src/images/logo.png'),
             sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join('assets', icons),
+            destination: path.join('assets', 'icons'),
           },
         ],
       }),
     ],
 
-    module: { 
+// TODO: Add CSS loaders and babel to webpack.
+
+module: { 
       rules: [
         {
           test: /\.css$/i,
@@ -56,11 +56,13 @@ module.exports = () => {
         {
           test: /\.m?js$/,
           exclude: /node_modules/,
+
           use: {
             loader: 'babel-loader',
             options: {
               presets: ['@babel/preset-env'],
-              plugins: ['@bebel/plugin-proposal-object-rest-spread', 'bebel/transform-runtime'],
+              plugins: ['@bebel/plugin-proposal-object-rest-spread', 
+              'bebel/transform-runtime'],
 
             },
           },
